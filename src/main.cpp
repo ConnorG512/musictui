5cf5684cb1839d8c74621cc045a674326dcf80a3
Connor Garey <connorgarey1999@gmail.com>
2025-12-25 21:52:05 +0000

    Open song provided

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,39 @@
 #include <print>
 #include <cassert>
 #include <AL/al.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
 
 auto main(int argc, const char* argv[]) -> int
 {
-  initscr();
-  keypad(stdscr, TRUE);
-
-  printw("%s", "MusicTUI!");
-  
+  // Get Music File
   if(argc < 2)
   {
     std::println("No music path provided!");
     endwin();
     return -1;
   }
+
+  const char * music_path {argv[1]};
+  
+  // Open file:
+  const auto song_open {open(music_path, O_RDONLY)};
+  if (song_open == -1)
+  {
+    std::println("No music path provided! Error: {}", strerror(errno));
+    endwin();
+    return -1;
+  }
+  
+  // Main Loop:
+  initscr();
+  keypad(stdscr, TRUE);
+
+  printw("%s", "MusicTUI!");
+  
   
   auto character {0};
   while((character = getch()) != 'q')
@@ -29,5 +48,7 @@ auto main(int argc, const char* argv[]) -> int
   }
 
   endwin();
+
+  close(song_open);
   return 0;
 }
