d25fe8910f4eb14b223e0fac6b5905c02414efea
Connor Garey <connorgarey1999@gmail.com>
2025-12-26 17:44:26 +0000

    Set volume of the track in the volume class

--- src/audio/volume.cpp
+++ src/audio/volume.cpp
@@ -2,14 +2,19 @@
 
 Audio::Volume::Volume(const ma_sound &sound)
   : sound_instance_{sound}
-  , current_volume_{ma_sound_get_volume(&sound_instance_)} {}
+  , current_volume_{ma_sound_get_volume(&sound_instance_)} 
+{
+  ma_sound_set_volume(&sound_instance_, current_volume_);
+}
 
 auto Audio::Volume::increaseVolume() noexcept -> void
 {
   current_volume_ += volume_increment_;
+  ma_sound_set_volume(&sound_instance_, current_volume_);
 }
 
 auto Audio::Volume::decreaseVolume() noexcept -> void
 {
   current_volume_ -= volume_increment_;
+  ma_sound_set_volume(&sound_instance_, current_volume_);
 }
