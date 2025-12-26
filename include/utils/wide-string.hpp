5001c948a82040b6d5691fbcc846c5e1649139f1
Connor Garey <connorgarey1999@gmail.com>
2025-12-26 21:25:38 +0000

    Added a constructor and writer method.

--- include/utils/wide-string.hpp
+++ include/utils/wide-string.hpp
@@ -3,6 +3,10 @@
 #include <cstdint>
 #include <array>
 #include <span>
+#include <stdexcept>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
 
 namespace Utils 
 {
@@ -10,10 +14,21 @@ namespace Utils
   class WideString
   {
     public: 
+     consteval explicit WideString(std::array<wchar_t, buffer_size> input_text)
+        : buffer_{writeBuffer(input_text)} {}
+
       auto getData() const noexcept -> std::span<const wchar_t>
       {
         return buffer_;
       }
+      auto writeBuffer(std::array<wchar_t, buffer_size> input_text) -> void
+      {
+        if(input_text.size() > buffer_size)
+          throw std::runtime_error("buffersize too big!");
+        
+        std::ranges::copy(buffer_.data(), input_text, sizeof(buffer_));
+      }
+
     private:
       std::array<wchar_t, buffer_size> buffer_{};
   };
