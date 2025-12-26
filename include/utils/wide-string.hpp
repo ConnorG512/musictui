14f07387ef65ef19cec69afda520c918943c18ba
Connor Garey <connorgarey1999@gmail.com>
2025-12-26 21:58:01 +0000

    Support for both const char* and wchar_t*

--- include/utils/wide-string.hpp
+++ include/utils/wide-string.hpp
@@ -7,22 +7,29 @@
 #include <algorithm>
 #include <iterator>
 #include <iostream>
+#include <concepts>
 
 namespace Utils 
 {
-  template <std::uint32_t buffer_size>
+  template<typename String, std::uint32_t buffer_size>
+  requires std::same_as<String, const char*> || std::same_as<String, const wchar_t*>
   class WideString
   {
     public: 
-     consteval explicit WideString(std::array<wchar_t, buffer_size> input_text)
+     constexpr explicit WideString(std::array<wchar_t, buffer_size> input_text)
         : buffer_{writeBuffer(input_text)} {}
 
       auto getData() const noexcept -> std::span<const wchar_t>
       {
         return buffer_;
       }
-      auto writeBuffer(std::array<wchar_t, buffer_size> input_text) -> void
+
+      auto writeBuffer(std::array<String, buffer_size> input_text) -> void
       {
+        if constexpr (std::same_as<String, const char*>)
+        {
+          mbstowcs(buffer_, input_text.data(), buffer_size);
+        }
         if(input_text.size() > buffer_size)
           throw std::runtime_error("buffersize too big!");
         
