bdeb584d81d2af19d87b0061aa138138432a5fa1
Connor Garey <connorgarey1999@gmail.com>
2025-12-26 16:43:26 +0000

    created Volume class

--- include/audio/volume.hpp
+++ include/audio/volume.hpp
@@ -7,9 +7,12 @@ namespace Audio
 class Volume
 {
   public: 
-    Volume(const ma_sound& sound);
+    Volume(const ma_sound &sound);
+    auto increaseVolume() noexcept -> void;
+    auto decreaseVolume() noexcept -> void;
 
   private:
+    ma_sound sound_instance_{};
     float current_volume_{0.0f};
     float volume_increment_{0.1f};
 };
