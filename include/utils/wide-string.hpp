fcecbecd085d37317d82a4c35eed9184fa899c59
Connor Garey <connorgarey1999@gmail.com>
2025-12-26 22:11:05 +0000

    Changed to concept instead of requires

--- include/utils/wide-string.hpp
+++ include/utils/wide-string.hpp
@@ -11,15 +11,18 @@
 
 namespace Utils 
 {
-  template<typename String, std::uint32_t buffer_size>
-  requires std::same_as<String, const char*> || std::same_as<String, const wchar_t*>
+  template<typename T> 
+  concept CharType = std::same_as<T, char> || std::same_as<T, wchar_t>
+
+  template<typename CharType, std::uint32_t buffer_size>
+  requires 
   class WideString
   {
     public: 
-     constexpr explicit WideString(std::array<wchar_t, buffer_size> input_text)
+     constexpr WideString(String input_text)
         : buffer_{writeBuffer(input_text)} {}
 
-      auto getData() const noexcept -> std::span<const wchar_t>
+      auto getData() const noexcept -> std::span<const String>
       {
         return buffer_;
       }
@@ -37,6 +40,6 @@ namespace Utils
       }
 
     private:
-      std::array<wchar_t, buffer_size> buffer_{};
+      std::array<String, buffer_size> buffer_{};
   };
 }
