02f2c58c6594037a86dbd72f1e4e010ef5f68c70
Connor Garey <connorgarey1999@gmail.com>
2025-12-26 17:45:29 +0000

    creating and setup tracks for the track class.

--- include/track_instance.hpp
+++ include/track_instance.hpp
@@ -1,16 +1,21 @@
 #pragma once 
 
 #include "audio/volume.hpp"
+#include "audio/engine.hpp"
 
 #include <miniaudio/miniaudio.h>
 
 class TrackInstance 
 {
   public: 
-    explicit TrackInstance(const char* track_path);
+    TrackInstance(const char* track_path, Audio::Engine& engine);
     ~TrackInstance();
   
   private:
-    ma_sound current_track_; 
-    Audio::Volume track_volume_;
+    ma_sound current_track_{}; 
+
+    auto createTrack(const char* track_path, Audio::Engine& engine) const -> ma_sound;
+
+  public:
+    Audio::Volume track_volume{current_track_};
 };
