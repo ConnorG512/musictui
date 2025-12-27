b0af06c228e9736e698c72c35ce03fbbb5e4c510
Connor Garey <connorgarey1999@gmail.com>
2025-12-27 14:46:09 +0000

    storing the cursor position.

--- include/playback-position.hpp
+++ include/playback-position.hpp
@@ -2,12 +2,16 @@
 
 #include <miniaudio/miniaudio.h>
 
+class TrackInstance;
+
 class PlaybackPosition 
 {
   public: 
-    auto setPosition(const ma_uint64 pos) noexcept -> void;
+    PlaybackPosition(ma_sound &track);
+    auto storeCurrentPCMPos() noexcept -> void; 
     auto getPosition() const noexcept -> const ma_uint64&;
 
   private:
+    ma_sound& current_track_instance_;
     ma_uint64 pcm_position_{0};
 };
--- src/playback-position.cpp
+++ src/playback-position.cpp
@@ -1,8 +1,12 @@
 #include "playback-position.hpp"
+#include "track_instance.hpp"
 
-auto PlaybackPosition::setPosition(const ma_uint64 pos) noexcept -> void 
+PlaybackPosition::PlaybackPosition(ma_sound &track)
+  : current_track_instance_{track} {}
+
+auto PlaybackPosition::storeCurrentPCMPos() noexcept -> void 
 {
-  pcm_position_ = pos;
+  ma_sound_get_cursor_in_pcm_frames(&current_track_instance_, &pcm_position_);
 }
 
 auto PlaybackPosition::getPosition() const noexcept -> const ma_uint64& 
