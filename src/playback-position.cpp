7f70239190d74f094464bd270f26a3c73f714134
Connor Garey <connorgarey1999@gmail.com>
2025-12-27 12:50:18 +0000

    Defined function in playback_position

--- src/playback-position.cpp
+++ src/playback-position.cpp
@@ -1,1 +1,11 @@
 #include "playback-position.hpp"
+
+auto PlaybackPosition::setPosition(const ma_uint64 pos) noexcept -> void 
+{
+  pcm_position_ = pos;
+}
+
+auto PlaybackPosition::getPosition() const noexcept -> const ma_uint64& 
+{
+  return pcm_position_;
+}
