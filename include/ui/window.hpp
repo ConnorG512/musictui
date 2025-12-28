38b90db8fbe4152db6cb9c9381fcfe65c340f6f4
Connor Garey <connorgarey1999@gmail.com>
2025-12-28 15:59:23 +0000

    Added constructor to Window class.

--- include/ui/window.hpp
+++ include/ui/window.hpp
@@ -8,11 +8,11 @@ namespace UI
   class Window
   {
     public:
-      Window() = default;
-      ~Window() = default;
+      Window(const int x, const int y);
 
     private:
-      auto createBoxedWindow() noexcept -> std::unique_ptr<WINDOW, decltype(&delwin)>;
+      auto createBoxedWindow(const int x = 10, const int y = 20) noexcept 
+        -> std::unique_ptr<WINDOW, decltype(&delwin)>;
       
       std::unique_ptr<WINDOW, decltype(&delwin)> window_instance_ {createBoxedWindow()}; 
   };
--- src/ui/window.cpp
+++ src/ui/window.cpp
@@ -1,8 +1,11 @@
 #include "ui/window.hpp"
 
-auto UI::Window::createBoxedWindow() noexcept -> std::unique_ptr<WINDOW, decltype(&delwin)> 
+UI::Window::Window(const int x, const int y)
+  : window_instance_{createBoxedWindow(x, y)} {}
+
+auto UI::Window::createBoxedWindow(const int x, const int y) noexcept -> std::unique_ptr<WINDOW, decltype(&delwin)> 
 {
-  WINDOW* created_window { newwin(20, 10, 0, 0)};
+  WINDOW* created_window { newwin(y, x, 0, 0)};
   box(created_window, 0, 0);
   wrefresh(created_window);
   
