ae406356286f728b602097e675e78950f1a5895f
Connor Garey <connorgarey1999@gmail.com>
2025-12-29 16:50:38 +0000

    Swapped public / private position.

--- include/ui/window.hpp
+++ include/ui/window.hpp
@@ -9,6 +9,14 @@ namespace UI
 {
 class Window
 {
+private:
+  auto createBoxedWindow(const std::optional<std::pair<int, int>> dimensions_xy,
+                         const std::pair<int, int> position_xy
+      ) noexcept
+      -> std::unique_ptr<WINDOW, decltype(&delwin)>;
+
+  std::unique_ptr<WINDOW, decltype(&delwin)> window_instance_{createBoxedWindow(std::nullopt, {0,0})};
+
 public:
   Window(const std::optional<std::pair<int, int>> dimensions_xy = std::nullopt,
          const std::pair<int, int> position_xy = {0,0});
@@ -18,13 +26,5 @@ public:
   
   auto ptr() const noexcept -> WINDOW*;
   auto cptr() const noexcept -> const WINDOW*;
-
-private:
-  auto createBoxedWindow(const std::optional<std::pair<int, int>> dimensions_xy,
-                         const std::pair<int, int> position_xy
-      ) noexcept
-      -> std::unique_ptr<WINDOW, decltype(&delwin)>;
-
-  std::unique_ptr<WINDOW, decltype(&delwin)> window_instance_{createBoxedWindow(std::nullopt, {0,0})};
 };
 } // namespace UI
