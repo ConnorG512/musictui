8f9aad04b1490d5939bcb1267ea114be765e57af
Connor Garey <connorgarey1999@gmail.com>
2025-12-30 14:01:03 +0000

    Rewrite using WINDOW pointer

--- include/ui/text-output.hpp
+++ include/ui/text-output.hpp
@@ -13,7 +13,7 @@ namespace UI
   namespace Text
   {
     auto drawStringsToScreen(
-        WINDOW& window_instance, std::span<const char*> messages, 
-        const std::pair<int, int> xy, std::optional<int> color = std::nullopt) noexcept -> void;
+        WINDOW* window_instance, std::span<const char*> messages, 
+        const std::span<std::pair<int, int>> xy, const std::optional<int> color = std::nullopt) noexcept -> void;
   }
 }
--- src/ui/text-output.cpp
+++ src/ui/text-output.cpp
@@ -2,23 +2,27 @@
 #include "ui/window.hpp"
 
 #include <cassert>
+#include <ranges>
 
 auto UI::Text::drawStringsToScreen(
-    WINDOW& window_instance, std::span<const char*> messages, 
-    const std::pair<int, int> xy, std::optional<int> color) noexcept -> void
+    WINDOW* window_instance, std::span<const char*> messages, 
+    const std::span<std::pair<int, int>> xy, const std::optional<int> color) noexcept -> void
 {
-  assert(messages.data() != nullptr);
-  
-  if(color.has_value())
-    wattron(&window_instance, color.value());
+  assert(messages.data() != nullptr && xy.data() != nullptr);
 
-  const auto& [x, y] = xy;
-  for(const auto& message : messages)
-  {
-    assert(message != nullptr);
-    mvprintw(y, x, "%s", message);
-  }
-  
   if(color.has_value())
-    wattroff(&window_instance, color.value());
+    wattron(window_instance, color.value());
+  
+  const auto& titles = std::views::zip(messages, xy);
+  for(const auto& [string, pos] : titles)
+  {
+    assert(string != nullptr);
+    const auto& [x_pos, y_pos] = pos;
+
+    mvprintw(y_pos, x_pos, "%s", string);
+  }
+
+  if(color.has_value())
+    wattroff(window_instance, color.value());
 }
+  
