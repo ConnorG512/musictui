36d6dae73bc5213487718579ed14766a2ecf35b6
Connor Garey <connorgarey1999@gmail.com>
2025-12-31 13:26:41 +0000

    Moved unique_ptr to private

--- include/read-directory.hpp
+++ include/read-directory.hpp
@@ -14,8 +14,9 @@ class Directory
     auto OpenDirectory(const char* music_directory) const 
       -> std::unique_ptr<DIR, decltype(&closedir)>;
 
-  public:
     std::unique_ptr<DIR, decltype(&closedir)> music_directory_ {
       opendir(std::format("{}{}", std::getenv("HOME"), "/music").c_str()), &closedir}; 
+  
+  public:
 
 };
