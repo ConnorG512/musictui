6086381320275d7bd0e9753e11063d662af934bd
Connor Garey <connorgarey1999@gmail.com>
2026-01-01 13:14:47 +0000

    ma_sound has been split into it's own class.

--- src/track/track-instance.cpp
+++ src/track/track-instance.cpp
@@ -3,35 +3,37 @@
 #include <cassert>
 
 TrackInstance::TrackInstance(const char *track_path, Audio::Engine &engine, int total_tracks, int current_track)
-    : current_track_{createTrack(track_path, engine)}, track_volume{current_track_}, track_position(current_track_)
+    : data_instance_{track_path, engine}
+    , track_volume{data_instance_.ref()}
+    , track_position(data_instance_.ref())
     , Switcher{total_tracks, current_track}
 {
 }
 
-auto TrackInstance::ref() noexcept -> ma_sound & { return current_track_; }
+auto TrackInstance::ref() noexcept -> ma_sound & { return data_instance_.ref(); }
 
-auto TrackInstance::playTrack() noexcept -> void { ma_sound_start(&current_track_); }
+auto TrackInstance::playTrack() noexcept -> void { ma_sound_start(&data_instance_.ref()); }
 
-auto TrackInstance::pauseTrack() noexcept -> void { ma_sound_stop(&current_track_); }
+auto TrackInstance::pauseTrack() noexcept -> void { ma_sound_stop(&data_instance_.ref()); }
 
 auto TrackInstance::stopTrack() noexcept -> void
 {
-  ma_sound_stop(&current_track_);
+  ma_sound_stop(&data_instance_.ref());
   track_position.setPosition(0);
 }
 
 auto TrackInstance::seekForward() noexcept -> void
 {
   track_position.adjustCursorPos(PlaybackPosition::PositionDirection::forward, 3);
-  ma_sound_seek_to_pcm_frame(&current_track_, track_position.getPosition());
-  ma_sound_start(&current_track_);
+  ma_sound_seek_to_pcm_frame(&data_instance_.ref(), track_position.getPosition());
+  ma_sound_start(&data_instance_.ref());
 }
 
 auto TrackInstance::seekBackward() noexcept -> void
 {
   track_position.adjustCursorPos(PlaybackPosition::PositionDirection::backward, 3);
-  ma_sound_seek_to_pcm_frame(&current_track_, track_position.getPosition());
-  ma_sound_start(&current_track_);
+  ma_sound_seek_to_pcm_frame(&data_instance_.ref(), track_position.getPosition());
+  ma_sound_start(&data_instance_.ref());
 }
 
 auto TrackInstance::swapTrack() noexcept -> void
