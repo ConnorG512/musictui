2d67fd4fe27414490abe3da35ba0a487b08eb96d
Connor Garey <connorgarey1999@gmail.com>
2026-01-01 16:26:16 +0000

    Constructor and destructor now public

--- include/audio/playing-sound.hpp
+++ include/audio/playing-sound.hpp
@@ -7,15 +7,15 @@ namespace Audio
   class PlayingSound 
   {
     private: 
-      PlayingSound(ma_engine& engine, const char* file_path);
-      ~PlayingSound();
-
       ma_engine& engine_;
       ma_sound sound_instance_{};
 
       auto CreateSound(ma_engine& engine, const char* file_path) const -> ma_sound;
 
     public:
+      PlayingSound(ma_engine& engine, const char* file_path);
+      ~PlayingSound();
+      
       auto clearSound() noexcept -> void;
       auto resetSound(const char* file_path);
   };
