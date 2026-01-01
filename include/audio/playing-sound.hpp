fbe2d2bd9a4055c845e04edd3599e27bdb7a6589
Connor Garey <connorgarey1999@gmail.com>
2026-01-01 16:23:38 +0000

    Added PlayingSound methods

--- include/audio/playing-sound.hpp
+++ include/audio/playing-sound.hpp
@@ -4,6 +4,19 @@
 
 namespace Audio 
 {
-  auto playSound(ma_engine& engine, const char* sound_path) noexcept -> void;
-  auto stopSound(ma_engine& engine, const char* sound_path) noexcept -> void;
+  class PlayingSound 
+  {
+    private: 
+      PlayingSound(ma_engine& engine, const char* file_path);
+      ~PlayingSound();
+
+      ma_engine& engine_;
+      ma_sound sound_instance_{};
+
+      auto CreateSound(ma_engine& engine, const char* file_path) const -> ma_sound;
+
+    public:
+      auto clearSound() noexcept -> void;
+      auto resetSound(const char* file_path);
+  };
 }
--- src/audio/playing-sound.cpp
+++ src/audio/playing-sound.cpp
@@ -2,16 +2,39 @@
 
 #include <miniaudio/miniaudio.h>
 #include <cassert>
+#include <stdexcept>
 
-auto Audio::playSound(ma_engine& engine, const char* sound_path) noexcept -> void
+Audio::PlayingSound::PlayingSound(ma_engine& engine, const char* file_path)
+  : engine_{engine}
+  , sound_instance_{CreateSound(engine_, file_path)} {}
+
+Audio::PlayingSound::~PlayingSound()
 {
-  assert(sound_path != nullptr);
-  ma_engine_play_sound(&engine, sound_path, nullptr);
+  clearSound();
 }
 
-auto Audio::stopSound(ma_engine& engine, const char* sound_path) noexcept -> void
+auto Audio::PlayingSound::CreateSound(ma_engine& engine, const char* file_path) const -> ma_sound
 {
-  assert(sound_path != nullptr);
-  ma_engine_stop(&engine);
+  assert(file_path != nullptr);
+  ma_sound sound {};
+
+  if(const auto result = ma_sound_init_from_file(&engine, file_path, 0, nullptr, nullptr, &sound);
+      result != MA_SUCCESS)
+  {
+    throw std::runtime_error("Failed to create sound instance from file!");
+  }
+  return sound;
 }
 
+auto Audio::PlayingSound::clearSound() noexcept -> void
+{
+  ma_sound_uninit(&sound_instance_);
+}
+
+auto Audio::PlayingSound::resetSound(const char* file_path)
+{
+  assert(file_path != nullptr);
+  
+  clearSound();
+  sound_instance_ = CreateSound(engine_, file_path);
+}
